bc8cfbd2a5cc5bccbff368613caedd20f5adc1f4
justin cechmanek <gj.cechmanek@gmail.com>
2021-03-05 21:15:24 -0800

    adds working quiz quiz game and problem set

---
+++ quiz/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"os"
+	"fmt"
+)
+
+func main() {
+	csvFilename := flag.String("csv", "problems.csv", "a csv file in th format of 'question,answer'")
+	flag.Parse()
+
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		fmt.Printf("failed to open csv file: %s\n", *csvFilename)
+		os.Exit(1)
+	}
+
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		fmt.Printf("failed to read csv file: %s\n", *csvFilename)
+		os.Exit(1)
+	}
+	problems := parseLines(lines)	
+	fmt.Println(problems)
+
+
+	correct := 0
+	for i, p := range problems {
+		fmt.Printf("problem # %d:, %s = \n", i+1, p.q)
+		var answer string
+		fmt.Scanf("%s\n", &answer) // gets rid of all spaces, read stdin into answer
+
+		if answer == p.a {
+			fmt.Print("correct!\n")
+			correct ++
+		}
+	}
+
+	fmt.Printf("You scored %d correct out of %d questions", correct, len(problems))
+} // end main
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{q: line[0], a: line[1]}
+	}
+	return ret
+}
+
+type problem struct {
+	q string
+	a string
+}
