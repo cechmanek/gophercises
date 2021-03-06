993559c114df1be684e2ea3e743b95cd5701a02e
justin cechmanek <gj.cechmanek@gmail.com>
2021-03-05 21:23:22 -0800

    adds comments to quiz script

--- quiz/main.go
+++ quiz/main.go
@@ -9,15 +9,17 @@ import (
 )
 
 func main() {
+	// add generic helper flags so you can pass args like "quiz.exe -csv problems.csv"
 	csvFilename := flag.String("csv", "problems.csv", "a csv file in th format of 'question,answer'")
 	flag.Parse()
 
-	file, err := os.Open(*csvFilename)
+	file, err := os.Open(*csvFilename) // csvFilename is a pointer, as per flag package standards
 	if err != nil {
 		fmt.Printf("failed to open csv file: %s\n", *csvFilename)
 		os.Exit(1)
 	}
 
+	// attempt to read in all problems
 	r := csv.NewReader(file)
 	lines, err := r.ReadAll()
 	if err != nil {
@@ -27,7 +29,7 @@ func main() {
 	problems := parseLines(lines)	
 	fmt.Println(problems)
 
-
+	// enumerate problems and ask user for answers. Also track correct responses
 	correct := 0
 	for i, p := range problems {
 		fmt.Printf("problem # %d:, %s = \n", i+1, p.q)
@@ -43,6 +45,7 @@ func main() {
 	fmt.Printf("You scored %d correct out of %d questions", correct, len(problems))
 } // end main
 
+// helper functions and structs
 func parseLines(lines [][]string) []problem {
 	ret := make([]problem, len(lines))
 	for i, line := range lines {
@@ -52,6 +55,6 @@ func parseLines(lines [][]string) []problem {
 }
 
 type problem struct {
-	q string
-	a string
+	q string // question
+	a string // answer
 }
