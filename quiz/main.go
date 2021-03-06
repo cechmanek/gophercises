ec1f1f81be1a6e9e0260db83aa0c5eaee7b9598d
justin cechmanek <gj.cechmanek@gmail.com>
2021-03-05 22:04:40 -0800

    uses good variable names, and styles to 80 char line limit

--- quiz/main.go
+++ quiz/main.go
@@ -9,19 +9,20 @@ import (
 )
 
 func main() {
-	// add generic helper flags so you can pass args like "quiz.exe -csv problems.csv"
-	csvFilename := flag.String("csv", "problems.csv", "a csv file in th format of 'question,answer'")
+	// generic helper flags so you can pass args like "quiz.exe -csv problems.csv"
+	csvFilename := flag.String("csv", "problems.csv",
+														 "a csv file in th format of 'question,answer'")
 	flag.Parse()
 
-	file, err := os.Open(*csvFilename) // csvFilename is a pointer, as per flag package standards
+	file, err := os.Open(*csvFilename) // csvFilename is a pointer
 	if err != nil {
 		fmt.Printf("failed to open csv file: %s\n", *csvFilename)
 		os.Exit(1)
 	}
 
 	// attempt to read in all problems
-	r := csv.NewReader(file)
-	lines, err := r.ReadAll()
+	reader := csv.NewReader(file)
+	lines, err := reader.ReadAll()
 	if err != nil {
 		fmt.Printf("failed to read csv file: %s\n", *csvFilename)
 		os.Exit(1)
@@ -31,30 +32,30 @@ func main() {
 
 	// enumerate problems and ask user for answers. Also track correct responses
 	correct := 0
-	for i, p := range problems {
-		fmt.Printf("problem # %d:, %s = \n", i+1, p.q)
+	for i, problem := range problems {
+		fmt.Printf("problem # %d:, %s = \n", i+1, problem.question)
 		var answer string
 		fmt.Scanf("%s\n", &answer) // gets rid of all spaces, read stdin into answer
 
-		if answer == p.a {
+		if answer == problem.answer {
 			fmt.Print("correct!\n")
 			correct ++
 		}
 	}
 
-	fmt.Printf("You scored %d correct out of %d questions", correct, len(problems))
+	fmt.Printf("You scored %d correct from %d questions", correct, len(problems))
 } // end main
 
 // helper functions and structs
 func parseLines(lines [][]string) []problem {
 	ret := make([]problem, len(lines))
 	for i, line := range lines {
-		ret[i] = problem{q: line[0], a: strings.TrimSpace(line[1])}
+		ret[i] = problem{question: line[0], answer: strings.TrimSpace(line[1])}
 	}
 	return ret
 }
 
 type problem struct {
-	q string // question
-	a string // answer
+	question string // question
+	answer string // answer
 }
